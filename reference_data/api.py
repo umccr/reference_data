7d528fa556bd497055cfddb1937ae97b86695b61
vladsaveliev <vladislav.sav@gmail.com>
2020-05-18 20:07:31 +1000

    Support s3 and gds paths to reference data, automatically download

--- reference_data/api.py
+++ reference_data/api.py
@@ -1,10 +1,10 @@
-import collections
 import os
-import socket
-from os.path import join, abspath, dirname, pardir, exists, isdir
+import tempfile
+from os.path import join, abspath, dirname, pardir, exists, isdir, isfile
 import re
 import sys
 import yaml
+from . import utils
 
 
 def package_path():
@@ -24,21 +24,16 @@ def load_paths():
     else:
         for ld in loc_by_name.values():
             if 'host_pattern' in ld:
-                if re.match(ld['host_pattern'], hostname):
-                    loc_dict = update_dict(loc_dict, ld)
+                if re.match(ld['host_pattern'], utils.hostname):
+                    loc_dict = utils.update_dict(loc_dict, ld)
                     break
     return loc_dict
 
 loc_dict = load_paths()
+
 name = loc_dict.get('name')
 genomes = loc_dict.get('genomes')
-genomes_dir = loc_dict.get('genomes_dir')
-cluster_cmd = loc_dict.get('cluster_cmd')
-cluster_jobscript = loc_dict.get('cluster_jobscript')
-try:
-    ncpus_on_node = loc_dict.get('ncpus_on_node', len(os.sched_getaffinity(0)))
-except:
-    ncpus_on_node = loc_dict.get('ncpus_on_node', os.cpu_count())
+genomes_dir = None
 
 
 def ref_file_exists(genome='all', key='fa', path=None):
@@ -52,11 +47,11 @@ def get_ref_file(genome='all', key='fa', path=None, must_exist=True):
     """ If path does not exist, checks the "genomes" dictionary for the location.
     """
     if path:
-        path = adjust_path(path)
+        path = utils.adjust_path(path)
         if exists(path):
-            return adjust_path(path)
+            return utils.adjust_path(path)
         else:
-            critical(f'Error: {path} is not found as file at {os.getcwd()}')
+            utils.critical(f'Error: {path} is not found as file at {os.getcwd()}')
 
     g_d = get_genomes_dict(genome)
     d = g_d
@@ -64,66 +59,104 @@ def get_ref_file(genome='all', key='fa', path=None, must_exist=True):
     for k in keys:
         d = d.get(k)
         if not d:
-            critical(f'Can\'t find refererence file [{".".join(keys)}] for genome "{genome}"'
-                     f' for host "{name or hostname}". Available keys: {", ".join(g_d)}')
+            utils.critical(f'Can\'t find refererence file [{".".join(keys)}] for genome "{genome}"'
+                     f' for host "{name or utils.hostname}". Available keys: {", ".join(g_d)}')
     if isinstance(d, str):
         path = d
     else:
-        critical(f'Error: path in genomes specified by keys [{".".join(keys)}] is not full.'
-                 f'Genome: {genome}, cwd: {os.getcwd()}, host: "{name or hostname}"')
+        utils.critical(f'Error: path in genomes specified by keys [{".".join(keys)}] is not full.'
+                 f'Genome: {genome}, cwd: {os.getcwd()}, host: "{name or utils.hostname}"')
 
     # Resolve found path:
     if path.startswith('genomes'):
         if genomes_dir:
             if not isdir(genomes_dir):
                 if must_exist:
-                    critical(f'Could not find the genomes directory {genomes_dir} for host {name or hostname}')
+                    utils.critical(f'Could not find the genomes directory {genomes_dir} for host '
+                             f'{name or utils.hostname}')
                 return None
             gd = genomes_dir
         else:
-            gd = find_genomes_dir(genomes_dir)
+            gd = find_genomes_dir()
         path = abspath(join(gd, pardir, path))
 
-    path = adjust_path(path)
+    path = utils.adjust_path(path)
     if not exists(path):
         if must_exist:
-            critical(f'Error: {path} does not exist for genome "{genome}" at host "{name or hostname}"')
+            utils.critical(f'Error: {path} does not exist for genome "{genome}" at host '
+                           f'"{name or utils.hostname}"')
 
     return path
 
 
 def get_genomes_dict(genome):
-    if genome not in genomes_dir:
-        critical(f'Error: genome {genome} not found for host "{name or hostname}". '
+    if genome not in genomes:
+        utils.critical(f'Error: genome "{genome}" not found for host "{name or utils.hostname}". '
                  f'Available: {", ".join(genomes)}')
     return genomes[genome]
 
 
-def find_genomes_dir(_genomes_dir=None):
+"""
+Finding genomes location
+
+A tool may be run with genomes_dir set explicitly via:
+    1. --genomes-dir
+    2. $UMCCRISE_GENOMES
+Or it can have a default for this machine in:
+    3. paths.yaml
+In these cases the path can be s3 or gds and needs downloading.
+
+If the path is not found anywhere, umccrise may attempt
+to find it:
+    4. umccrise installation folder under umccrise/genomes
+    5. current package installation folder under umccrise/genomes
+    6. extras/umccrise/genomes
+In these cases the path doesn't need downloading and can be used directly.
+
+Once the path is found and downloaded, .genomes_dir propery is set and can be used directly.
+"""
+
+
+def find_genomes_dir(_new_genomes_url=None):
     """
     Trying:
-    1. genomes_dir (when provided explictily in paths.yaml or with --genomes-dir)
-    2. if umccrise installation under umccrise/genomes
-    2. at extras/umccrise/genomes
+    1. --genome-dir
+    2. $UMCCRISE_GENOMES
+    3. paths.yaml
+    4. umccrise installation folder under umccrise/genomes
+    5. current package installation folder under umccrise/genomes
+    6. extras/umccrise/genomes
     """
+    global genomes_dir
+
     tried = []
 
-    if genomes_dir:
+    if _new_genomes_url:
+        # setting up a new genomes location (likely provided by --genomes), even if it was set before
+        utils.info(f'Using genomes location that was provided explicitly '
+                   f'(in paths.yaml or with --genomes): {_new_genomes_url}')
+        genomes_dir = _set_up_new_genomes_dir(_new_genomes_url)
         return genomes_dir
-    tried.append(f'genomes_dir in hpc_utils/paths.yaml for location {name or hostname}')
+    tried.append(f'--genomes option')
 
-    if _genomes_dir and isdir(_genomes_dir):
-        info('Using genomes dir that was provided explicitly (in paths.yaml or with --genomes-dir)')
-        return _genomes_dir
-    tried.append(f'--genomes-dir option')
+    if genomes_dir is not None:
+        # `find_genomes_dir` was called once before, and the genomes dir
+        # is already found and/or downloaded locally and can be used directly:
+        return genomes_dir
 
     if os.environ.get('UMCCRISE_GENOMES') is not None:
         gd = os.environ.get('UMCCRISE_GENOMES')
-        if not isdir(gd):
-            critical(f'Directory $UMCCRISE_GENOMES={gd} does not exist or not a dir')
-        info('Using genomes dir set by $UMCCRISE_GENOMES env var')
-        return gd
+        utils.info(f'Using genomes location set by $UMCCRISE_GENOMES environment variable: {gd}')
+        genomes_dir = _set_up_new_genomes_dir(gd)
+        return genomes_dir
     tried.append(f'$UMCCRISE_GENOMES env var')
+
+    if 'genomes_dir' in loc_dict:
+        gd = loc_dict.get()
+        utils.info(f'Using the genomes location from reference_data/paths.yml: {gd}')
+        genomes_dir = _set_up_new_genomes_dir(gd)
+        return genomes_dir
+    tried.append(f'genomes_dir in paths.yaml for location {name or utils.hostname}')
 
     try:
         from umccrise import package_path as umccrise_path
@@ -135,164 +168,70 @@ def find_genomes_dir(_genomes_dir=None):
         gd2 = abspath(join(umccrise_path(), pardir, pardir, 'genomes'))
         gd = gd if isdir(gd) else gd2
         if isdir(gd):
-            info(f'Using genomes dir at umccrise package parent folder')
-            return gd
+            utils.info(f'Using genomes dir at umccrise package parent folder')
+            genomes_dir = gd
+            return genomes_dir
         tried.append(f'umccrsie package parent folder ({gd})')
 
     gd = abspath(join(package_path(), pardir, 'genomes'))
     if isdir(gd):
-        info(f'Using genomes dir at hpc_utils package parent folder')
-        return gd
+        utils.info(f'Using genomes dir at hpc_utils package parent folder')
+        genomes_dir = gd
+        return genomes_dir
     tried.append(f'hpc_utils package parent folder ({gd})')
 
     extras = loc_dict.get('extras')
     if extras and isdir(extras):
         gd = join(extras, 'umccrise', 'genomes')
         if isdir(gd):
-            info(f'Using genomes dir at production installation of "umccrise" extras')
-            return gd
+            utils.info(f'Using genomes dir at production installation of "umccrise" extras')
+            genomes_dir = gd
+            return genomes_dir
         tried.append(f'production installation of "umccrise" extras ({gd})')
 
-    critical(f'Cannot find "genomes" folder. Tried: {", ".join(tried)}')
+    utils.critical(f'Cannot find "genomes" folder. Tried: {", ".join(tried)}')
 
 
-def set_genomes_dir(new_genomes_dir=None):
-    global genomes_dir
-    if new_genomes_dir:
-        # genomes_dir was provided explicitly (in paths.yaml or with --genomes-dir)
-        verify_dir(new_genomes_dir, is_critical=True)
-        genomes_dir = new_genomes_dir
-    else:
-        genomes_dir = find_genomes_dir()
-    if not genomes_dir:
-        critical('Could not detect genomes dir. Please specify one with --genomes-dir or $UMCCRISE_GENOMES')
+def _set_up_new_genomes_dir(genomes_url=None):
+    target_genomes_dir = utils.safe_mkdir(utils.adjust_path('~/umccrise_genomes'))
+    from . import _version
+    version = _version.__version__
+    versioned_genomes_url = f'{genomes_url}_{version.replace(".", "")}'
 
+    if isfile(genomes_url) and genomes_url.endswith('.tar.gz') or genomes_url.endswith('.tgz'):
+        return utils.extract_tarball_input(genomes_url, target_genomes_dir)
 
-def secondary_conda_env(env_name='pcgr', is_critical=False):
-    py_path = sys.executable  # e.g. /miniconda/envs/umccrise/bin/python
-    env_path = dirname(dirname(py_path))  # e.g. /miniconda/envs/umccrise
-    env_path = env_path + '_' + env_name  # e.g. /miniconda/envs/umccrise_pcgr
-    if not isdir(env_path):
-        if is_critical:
-            critical(f'Can\'t find environment {env_path}')
-        else:
-            return None
-    return env_path
-
-
-#######################
-######## Utils ########
-#######################
-
-def get_hostname():
-    return os.environ.get('HOSTNAME') or os.environ.get('HOST') or socket.gethostname()
-
-hostname = get_hostname()
-
-
-# Expand paths beginning with '~' or '~user'.
-# '~' means $HOME; '~user' means that user's home directory.
-# If the path doesn't begin with '~', or if the user or $HOME is unknown,
-# the path is returned unchanged (leaving error reporting to whatever
-# function is called with the expanded path as argument).
-# See also module 'glob' for expansion of *, ? and [...] in pathnames.
-# (A function should also be defined to do full *sh-style environment
-# variable expansion.)
-def expanduser(path):
-    """Expand ~ and ~user constructs.
-
-    If user or $HOME is unknown, do nothing."""
-    if path[:1] != '~':
-        return path
-    i, n = 1, len(path)
-    while i < n and path[i] not in '/\\':
-        i = i + 1
-
-    if 'HOME' in os.environ:
-        userhome = os.environ['HOME']
-    elif 'USERPROFILE' in os.environ:
-        userhome = os.environ['USERPROFILE']
-    elif not 'HOMEPATH' in os.environ:
-        return path
-    else:
+    elif genomes_url.startswith('s3://'):
+        if not genomes_url.endswith('/'):
+            genomes_url = genomes_url + '/'
         try:
-            drive = os.environ['HOMEDRIVE']
-        except KeyError:
-            drive = ''
-        userhome = join(drive, os.environ['HOMEPATH'])
+            utils.run_simple(f'aws s3 ls {genomes_url}')
+        except:
+            try:
+                utils.run_simple(f'aws s3 ls {versioned_genomes_url}')
+            except:
+                utils.critical(f'Cannot find reference data at s3 URL {genomes_url} or {versioned_genomes_url}')
+            else:
+                genomes_url = versioned_genomes_url
+        utils.run_simple(f'aws s3 sync {genomes_url} {target_genomes_dir}')
+        return target_genomes_dir
 
-    if i != 1:  # ~user
-        userhome = join(dirname(userhome), path[1:i])
+    elif genomes_url.startswith('gds://'):
+        if not genomes_url.endswith('/'):
+            genomes_url = genomes_url + '/'
+        try:
+            utils.run_simple(f'iap folders list {genomes_url} | grep -v -q "No folders found"')
+        except:
+            try:
+                utils.run_simple(f"iap folders list {versioned_genomes_url} | grep -v -q \"No folders found\"")
+            except:
+                utils.critical(f'Cannot find reference data at GDS URL {genomes_url} or {versioned_genomes_url}')
+            else:
+                genomes_url = versioned_genomes_url
+        utils.run_simple(f"iap files download {genomes_url}/* {target_genomes_dir}")
+        return target_genomes_dir
 
-    return userhome + path[i:]
+    else:
+        # a local directory?
+        return utils.verify_dir(genomes_url, is_critical=True)
 
-
-def remove_quotes(s):
-    if s and s[0] in ['"', "'"]:
-        s = s[1:]
-    if s and s[-1] in ['"', "'"]:
-        s = s[:-1]
-    return s
-
-
-def adjust_path(path):
-    if path is None: return None
-
-    path = remove_quotes(path)
-    if path is None: return None
-
-    path = expanduser(path)
-    if path is None: return None
-
-    path = abspath(path)
-    if path is None: return None
-
-    return path.replace('//', '/')
-
-
-def verify_dir(dirpath, description='', silent=False, is_critical=False):
-    if dirpath is None:
-        msg = (description + ': i' if description else 'I') + 's not specified (None).'
-        info(msg, silent, is_critical)
-        return dirpath
-
-    if not dirpath:
-        msg = (description + ': d' if description else 'D') + 'ir name is empty.'
-        info(msg, silent, is_critical)
-        return None
-
-    dirpath = adjust_path(dirpath)
-    if not exists(dirpath):
-        msg = (description + ': ' if description else '') + dirpath + ' does not exist.'
-        info(msg, silent, is_critical)
-        return None
-
-    if not isdir(dirpath):
-        msg = (description + ': ' if description else '') + dirpath + ' is not a directory.'
-        info(msg, silent, is_critical)
-        return None
-
-    return dirpath
-
-
-def critical(msg):
-    sys.stderr.write(msg + '\n')
-    sys.exit(1)
-
-
-def info(msg, silent=False, is_critical=False):
-    if is_critical:
-        critical(msg)
-    if not silent:
-        sys.stderr.write(msg + '\n')
-
-
-def update_dict(to_update, updates):
-    """ Recursively updates a nested dict `to_update` from a nested dict `updates`
-    """
-    for key, val in updates.items():
-        if isinstance(val, collections.Mapping):
-            to_update[key] = update_dict(to_update.get(key, {}), val)
-        else:
-            to_update[key] = updates[key]
-    return to_update
