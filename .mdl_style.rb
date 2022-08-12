4b28f18a1b5442a82ee6057433c7dc06fafb2f9a
Stephen Watts <hello@stephen.ac>
2022-08-12 12:17:31 +1000

    Ignore code block line length for markdown linting

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -3,4 +3,5 @@ exclude_rule 'MD033' # Allow inline HTML
 exclude_rule 'MD041' # Do not require first line to be top level header
 rule 'MD013', :line_length => 120 # Set line max length to 120
 rule 'MD013', :tables => false # Disable line length on tables
+rule 'MD013', :code_blocks => false # Ignore line lengths within code blocks
 rule 'MD024', :allow_different_nesting => true # Allow same headings in different nesting/scopes
